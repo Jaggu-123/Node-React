dd2a987d5acbff3b00d6369cf3ddeb7b8c4d9c57
Jaggu-123 <manishjagnani1231999@gmail.com>
2018-08-31 19:12:39 +0530

    Added Passport Stuff

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+// we are making a new instance for authentication with google
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken);
+        console.log(refreshToken);
+        console.log(profile);
+    })
+);
+
+
+//creating a route handler for pushing the user for auth process whenever he direct to /auth/google //
+app.get('/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']             //we are demanding for user profile and email
+    })
+);
+
+//creating a route handler to pushback the route with incoming code from above route for accessing user profile//
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); 
